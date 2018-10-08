a2681e505a0e7626761d27226e76ef1b3ff4e322
fmchora <fmchora@gmail.com>
2018-10-07 22:30:29 -0600

    Added audio to the rocket

--- Rocket Lander/Assets/Rocket.cs
+++ Rocket Lander/Assets/Rocket.cs
@@ -4,37 +4,47 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
-    Rigidbody rigidBody;
-    // Use this for initialization
-    void Start ()
-    {
-        rigidBody = GetComponent<Rigidbody>();
+	Rigidbody rigidBody;
+	AudioSource audioSource;
+	// Use this for initialization
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        ProcessInput();
 	}
 
-    private void ProcessInput()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            print("Thrusting");
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            print("Rotating left");
-            transform.Rotate(Vector3.forward);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            print("Rotating Right");
-            transform.Rotate(-Vector3.forward);
-        }
+	// Update is called once per frame
+	void Update ()
+	{
+		ProcessInput();
+	}
 
-    }
+	private void ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			print("Thrusting");
+			rigidBody.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			}
+
+		}
+		else
+			audioSource.Stop();
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			print("Rotating left");
+			transform.Rotate(Vector3.forward);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			print("Rotating Right");
+			transform.Rotate(-Vector3.forward);
+		}
+
+	}
 }
